e254b63e880fbedcbb159ba3929fef19b06aeb18
bkozhaev <bkozhaev@gmail.com>
2018-04-27 00:16:31 +0400

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  helper_method :current_user_can_edit?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+  end
+
 end
