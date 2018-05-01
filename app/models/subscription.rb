c04b01048624978ba731a423bfae687c9e2e1586
bkozhaev <bkozhaev@gmail.com>
2018-05-01 14:49:00 +0400

    refactoring if and unless

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,11 +4,11 @@ class Subscription < ApplicationRecord
 
   validates :event, presence: true
 
-  validates :user_name, presence: true, unless: 'user.present?'
-  validates :user_email, presence: true, format: /\A[a-zA-z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: 'user.present?'
+  validates :user_name, presence: true, unless: -> {user.present?}
+  validates :user_email, presence: true, format: /\A[a-zA-z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless:-> {user.present?}
 
-  validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
-  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+  validates :user, uniqueness: {scope: :event_id}, if: -> {user.present?}
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> {user.present?}
   def user_name
     if user.present?
       user.name
