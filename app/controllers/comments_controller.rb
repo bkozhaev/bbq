0e9ba271fb7da5512e70ec81774cd0574c54a8f4
bkozhaev <bkozhaev@gmail.com>
2018-05-07 22:37:40 +0400

    event-mailer, sendgrid

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
+      notify_subscribers(@event, @new_comment)
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
       render 'events/show', alert: I18n.t('controllers.comments.error')
@@ -31,13 +32,25 @@ class CommentsController < ApplicationController
   def set_event
     @event = Event.find(params[:event_id])
   end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = @event.comments.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def comment_params
-      params.require(:comment).permit(:body, :user_name)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = @event.comments.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def comment_params
+    params.require(:comment).permit(:body, :user_name)
+  end
+
+  def notify_subscribers(event, comment)
+    # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
+    # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
     end
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'baur.kozhaev@gmail.com'
   layout 'mailer'
 end
