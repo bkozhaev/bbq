0864b256549b09f64cdc856f820a76d512c7282c
bkozhaev <bkozhaev@gmail.com>
2018-05-07 22:45:05 +0400

    mailing refactoring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
       address: 'smtp.gmail.com',
       port: '587',
       user_name: 'baur.kozhaev',
-      password: ,
+      password: '',
       authentication: 'plain',
       enable_starttls_auto: true
   }
