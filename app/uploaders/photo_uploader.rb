d820762c65f86a501a91f1898faf673bdec84859
bkozhaev <bkozhaev@gmail.com>
2018-05-07 12:51:17 +0400

    deletting olgas suggestion

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,13 +4,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.production?
-  #   storage :fog
-  # else
-  #   storage :file
-  # end
-  #
-  include Cloudinary::CarrierWave
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
