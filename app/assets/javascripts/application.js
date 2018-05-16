aa8a7539af32e3bcd28b99014ed31137174b0def
bkozhaev <bkozhaev@gmail.com>
2018-05-16 09:15:03 +0400

    map label refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
-//= require twitter/bootstrap/alert
+//= require map.js
 //= require_tree
-//= require map
